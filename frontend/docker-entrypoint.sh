2cac7eb0a750f3cf147d062a780b56a9a31d8fd9
khiempg <khiem.pg225868@sis.hust.edu.vn>
2026-06-26 15:31:11 +0700

    Fix Kubernetes frontend API routing

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -6,11 +6,11 @@ cp -R /opt/app/html/. /tmp/nginx/html/
 
 cat > /tmp/nginx/html/env.js <<EOF
 window.__APP_CONFIG__ = {
-  APP_API_BASE_URL: "${APP_API_BASE_URL:-http://localhost:5002/api}",
-  APP_API_AUTH_URL: "${APP_API_AUTH_URL:-http://localhost:5001/api}",
-  APP_API_DELIVERY_URL: "${APP_API_DELIVERY_URL:-http://localhost:5002/api}",
-  APP_API_ORDER_URL: "${APP_API_ORDER_URL:-http://localhost:5003/api}",
-  APP_API_NOTIFICATION_URL: "${APP_API_NOTIFICATION_URL:-http://localhost:5004/api}"
+  APP_API_BASE_URL: "${APP_API_BASE_URL:-/api}",
+  APP_API_AUTH_URL: "${APP_API_AUTH_URL:-/api}",
+  APP_API_DELIVERY_URL: "${APP_API_DELIVERY_URL:-/api}",
+  APP_API_ORDER_URL: "${APP_API_ORDER_URL:-/api}",
+  APP_API_NOTIFICATION_URL: "${APP_API_NOTIFICATION_URL:-/api}"
 };
 EOF
 
